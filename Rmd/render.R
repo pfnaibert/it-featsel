257e49ee4f7d6e9ad5126d36803a2445f388c6e3
pfnaibert <paulo.naibert@gmail.com>
2020-06-13 13:54:33 -0300

    new folder organization

--- Rmd/render.R
+++ Rmd/render.R
@@ -2,11 +2,12 @@
 #!/usr/bin/env Rscript
 
 # Rscript --vanilla filename.R input
+# Rscript  filename.R input
 
 # 
 args = commandArgs(trailingOnly=TRUE)
 
 
 ## program
-rmarkdown::render(args[1])
+rmarkdown::render(args[1], output_dir="../html")
 
