75aa2d7bba8a09c66b99663398fb5c2b136c1f5a
pfnaibert <paulo.naibert@gmail.com>
2020-07-30 12:48:21 -0300

    changed the plot script

--- R/06-plots-SPY.R
+++ R/06-plots-SPY.R
@@ -1,15 +1,15 @@
 #!/usr/bin/env Rscript
 # retport
 #####################################
-source("./funs-ports-IT.R")
-source("./funs-plots.R")
+source("../funs/funs-ports-IT.R")
+source("../funs/funs-plots.R")
 library(knitr)
 J <- 500; win1 <- seq(1, J)
 W <- 250
 
 #####################################
 # Import
-ports <- readRDS("./data/spy5-rps.rds")
+ports <- readRDS("../data/spy5-rps.rds")
 str(ports); head(ports$ac.rps)
 
 mkt    <- ports$mkt[-win1]
@@ -18,37 +18,22 @@ rps.ac <- ports$ac.rps
 rpn.ac <- ports$ac.rpn
 
 #####################################
-portnames <- c("bwd.30", "bwd.40", "fwd.30", "fwd.40", "las.20")
+portnames <- c("las.20", "bwd.40", "fwd.20")
 n <- length(portnames); n
-
-#####################################
-# PLOTS 
-source("./funs-plots.R")
 
 #####################################
 # FIGURES
 for(i in 1:n)
 {
-pdf(file = paste0("../FIGS/SP500-retac-", portnames[i], ".pdf"), width=8, height=7*sqrt(2))
+pdf(file = paste0("../figs/SP500-retac-", portnames[i], ".pdf"), width=8, height=7*sqrt(2))
 par(mfrow = c(2,1))
 # par(mar=c(2.5, 2, 2, 0.2))
 # par(mar=c(4, 4, 2, 0.4))
 {
-plot.ac2(cbind(mkt.ac, rps.ac[,grep(portnames[i], colnames(rps.ac))]), "SP500",
-         paste("Cumulative Returns of the", portnames[i], "Portfolio without TC") )
 plot.ac2(cbind(mkt.ac, rpn.ac[,grep(portnames[i], colnames(rps.ac))]), "SP500",
-         paste("Cumulative Returns of the", portnames[i], "Portfolio with TC") )
-}
-dev.off()
-
-pdf(file = paste0("../FIGS/SP500-vol-", portnames[i], ".pdf"), width = 8, height=7*sqrt(2))
-par(mfrow = c(2,1))
-# par(mar=c(4, 4, 2, 0.4))
-{
-plot.vol(cbind(mkt, ports$rps[,grep(portnames[i], colnames(rps.ac))]),
-        "SP500", paste("SD of returns of the", portnames[i], "portfolio without TC (trailing 250 days)"), W)
+         paste("Cumulative Net Returns of the", portnames[i], "Portfolio") )
 plot.vol(cbind(mkt, ports$rpn[,grep(portnames[i], colnames(rps.ac))]),
-        "SP500", paste("SD of returns of the", portnames[i], "portfolio with TC (trailing 250 days)"), W)
+        "SP500", paste("SD of Net Returns of the", portnames[i], "portfolio (trailing 250 days)"), W)
 }
 dev.off()
 }
